85a8a978a134679e233731faf7da03a85d4fb9e7
Dusky <charlieyeudall@gmail.com>
2022-05-31 20:24:18 -0600

    added keyboardlisteners, implemented v0.02 changes

--- Main.py
+++ Main.py
@@ -2,16 +2,33 @@ import time
 import os
 import keyboard
 resource = 0
-workers = 1
+workers = 0
 workerability = 1
+workercost = 20
+displayworkerwarning = 0
 
 
-def buyworkers():
+def keyboardlistners():
+    keyboard.on_release_key("space", playerfarmresource)
+    keyboard.on_release_key("w", buyworkers)
+
+
+def playerfarmresource(self):
+    global resource
+    resource += 1
+
+
+def buyworkers(self):
     global workers
     global resource
-    if resource > 20:
-        resource -= 20
+    global workercost
+    global displayworkerwarning
+    if resource >= workercost:
+        resource -= workercost
         workers += 1
+        workercost *= 2
+    else:
+        displayworkerwarning = 20
 
 
 def updateworkers():
@@ -19,18 +36,32 @@ def updateworkers():
     resource += (workers * workerability)
 
 
-def update():
-    updateworkers()
-    buyworkers()
+def display():
+    global resource
+    global displayworkerwarning
     os.system('cls')
     print("You currently have", workers, "workers.\n")
     print("You currently have", resource, "resource.")
-    pass
+    print("Press Spacebar to farm resource!\n")
+    if resource >= workercost or workers > 0:
+        print("Press 'W' to buy a worker for", workercost, "resource.")
+    if displayworkerwarning > 0:
+        if resource < workercost:
+            print("Not enough resource!")
+            displayworkerwarning -= 1
+        else:
+            displayworkerwarning = 0
+
+
+def update():
+    updateworkers()
 
 
 def main():
+    keyboardlistners()
     while True:
         update()
+        display()
         time.sleep(.04)
 
 
