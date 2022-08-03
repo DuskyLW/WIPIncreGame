13f84626b82efd16e3650ef67173e96f8ff89afb
Dusky <charlieyeudall@gmail.com>
2022-08-02 21:53:33 -0600

    Added comments to Resource class

--- Resource.py
+++ Resource.py
@@ -1,5 +1,8 @@
 class Resource:
 
+    # Takes a name, amount, amount cap and flag for denoting if you would like
+    # plural added to your amount to display sentence. If the cap less than 0,
+    # set it to an infinite float, otherwise make it the cap argument.
     def __init__(self, name, amount, cap, plural=False):
         self.name = name
         if cap < 0:
@@ -10,9 +13,14 @@ class Resource:
         self.warning = 0
         self.plural = plural
 
+# Takes amount to add as arg and checks it against the cap, if the amount can
+# be added before hitting the cap, add it, otherwise choose cap.
     def add(self, amount):
         self.amount = min(self.cap, self.amount+amount)
 
+# Takes amount to subtract as arg and checks it against current amount, if
+# amount to subtract is less than or equal to current amount, subtract it. Otherwise,
+# check warning flag for true then make the tick count 20.
     def spend(self, amount, warning=True):
         if amount <= self.amount:
             self.amount -= amount
@@ -22,9 +30,15 @@ class Resource:
                 self.warning = 20
             return False
 
+# Takes an amount as arg and checks if the amount is less than or equal to
+# current amount, returns boolean based on evaluation.
     def canAfford(self, amount):
         return amount <= self.amount
 
+# If amount is greater than or equal to 1 and plural flag is true, return a
+# string for what you have with a plural, else return a string as a singular
+# sentence. If the warning tick is greater than 0, display the warning and
+# decrement the warning tick by 1 every update.
     def display(self):
         if (self.amount >= 1 and self.plural):
             print("You have {amount} {name}s".format(
@@ -36,21 +50,28 @@ class Resource:
             print("You do not have enough {name}!".format(name=self.name))
             self.warning -= 1
 
+# Can be called and returns the current amount.
     def getAmount(self):
         return self.amount
 
+# Takes an amount as arg and sets the amount to that if it doesn't exceed
+# the cap, otherwise it returns the cap.
     def setAmount(self, amount):
         self.amount = min(amount, self.cap)
 
+# Can be called and returns the current set cap for the resource.
     def getCap(self):
         return self.cap
 
+# Takes an amount as arg and if the number is a negative, make the cap infinite,
+# otherwise set the arg as the cap.
     def setCap(self, cap):
         if cap < 0:
             self.cap = float('inf')
         else:
             self.cap = cap
 
+# Gives a printable representation of the resource class
     def __repr__(self):
         return self.name + " Resource"
 
